0f5e5900466d5d39b26939cda5df68d92c0c165a
Justas <jmedeisis@gmail.com>
2014-08-31 19:24:51 +0100

    Updated sample with a visualization of the current device orientation in an ActionBar icon. Changed demo images.

--- sample/src/main/java/com/jmedeisis/example/windowview/DemoActivity.java
+++ sample/src/main/java/com/jmedeisis/example/windowview/DemoActivity.java
@@ -1,11 +1,15 @@
 package com.jmedeisis.example.windowview;
 
+import android.annotation.TargetApi;
 import android.content.pm.ActivityInfo;
-import android.support.v7.app.ActionBarActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.jmedeisis.windowview.WindowView;
 
@@ -25,18 +29,19 @@ public class DemoActivity extends ActionBarActivity {
 
         windowView1 = (WindowView) findViewById(R.id.windowView1);
         windowView2 = (WindowView) findViewById(R.id.windowView2);
-        windowView1.setOnClickListener(new View.OnClickListener() {
+        /*
+         * For sake of demo interface simplicity & compass visualisation, tapping on either
+         * WindowView resets both of their orientation origins.
+         * Typically, this would be done individually.
+         */
+        View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                windowView1.resetOrientationOrigin();
+                resetWindowViewOrientationOrigins();
             }
-        });
-        windowView2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                windowView2.resetOrientationOrigin();
-            }
-        });
+        };
+        windowView1.setOnClickListener(onClickListener);
+        windowView2.setOnClickListener(onClickListener);
 
         if(null != savedInstanceState && savedInstanceState.containsKey(ORIENTATION)
                 && savedInstanceState.containsKey(DEBUG_TILT)
@@ -60,6 +65,7 @@ public class DemoActivity extends ActionBarActivity {
         outState.putBoolean(DEBUG_IMAGE, debugImage);
     }
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.demo, menu);
@@ -69,7 +75,53 @@ public class DemoActivity extends ActionBarActivity {
         menu.findItem(R.id.action_debug_tilt).setChecked(debugTilt);
         menu.findItem(R.id.action_debug_image).setChecked(debugImage);
 
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+            final View actionView = View.inflate(this, R.layout.device_compass, null);
+            final View xy = actionView.findViewById(R.id.compass_xy);
+            final View z = actionView.findViewById(R.id.compass_z);
+            windowView1.setOnNewOrientationListener(new WindowView.OnNewOrientationListener() {
+                @Override
+                public void onNewOrientation(WindowView windowView) {
+                    xy.setRotation(windowView.getLatestYaw());
+                    xy.setRotationX(windowView.getLatestPitch());
+                    xy.setRotationY(windowView.getLatestRoll());
+
+                    z.setRotation(windowView.getLatestYaw());
+                    z.setRotationX(windowView.getLatestPitch());
+                    z.setRotationY(windowView.getLatestRoll() - 90);
+                }
+            });
+            actionView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    resetWindowViewOrientationOrigins();
+                }
+            });
+            actionView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    Toast t = Toast.makeText(DemoActivity.this,
+                            R.string.action_reset_orientation, Toast.LENGTH_SHORT);
+                    int[] pos = new int[2];
+                    actionView.getLocationInWindow(pos);
+                    t.setGravity(Gravity.TOP | Gravity.LEFT,
+                            pos[0], pos[1] + actionView.getHeight() / 2);
+                    t.show();
+                    return true;
+                }
+            });
+
+
+            menu.findItem(R.id.action_reset_orientation).setActionView(actionView);
+        }
+
         return true;
+    }
+
+    private void resetWindowViewOrientationOrigins(){
+        windowView1.resetOrientationOrigin();
+        windowView2.resetOrientationOrigin();
+        Toast.makeText(DemoActivity.this, R.string.hint_orientation_reset, Toast.LENGTH_SHORT).show();
     }
 
     @Override
